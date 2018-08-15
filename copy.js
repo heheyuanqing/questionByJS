f82ed04064a2d672c3c75c12f5c09a2d264781bc
heheyuanqing <2570394541@qq.com>
2018-08-15 16:20:00 +0800

    题目

---
+++ copy.js
@@ -0,0 +1,41 @@
+/*
+* 实现深复制
+* */
+
+function deepCopy(inital,final){
+    var newObj = final||{};
+
+    for(var i in inital){
+        var prop = inital[i];
+        if(prop===newObj){
+            continue;
+        }
+        if(typeof inital[i]==='object'){
+            newObj[i]=inital[i].constructor===Array?[]:{};
+            deepCopy(inital[i],newObj[i]);
+        }
+        else{
+            newObj[i]=inital[i];
+        }
+    }
+    return newObj;
+}
+
+function deepClone(obj){
+    var newObj;
+    if(typeof obj==='object'){
+        newObj = obj.constructor===Array?[]:{};
+        for(var i in obj){
+            if(typeof obj[i] === 'object'){
+                newObj=deepClone(obj[i]);
+            }
+            else{
+                newObj[i]=obj[i]
+            }
+        }
+    }
+    else{
+        newObj=obj;
+    }
+    return newObj;
+}
---
+++ reduce.js
@@ -0,0 +1,32 @@
+Array.prototype.makeReducer=function(){
+    if (arguments.length < 1) {
+        throw "need some arg";
+    }
+    let pre, fn;
+    if (arguments.length === 2) {
+        pre = arguments[0];
+        fn = arguments[1];
+    }
+    else fn = arguments[0];
+    if (typeof fn !== 'function') {
+        throw fn + "is not a function";
+    }
+
+    let arr=this,len=this.length,index=1,cur;
+
+    if(index<=len&&index>1){
+        pre = arr[index-1];
+    }
+
+    for(;index<len;index++){
+        cur=arr[index];
+        pre = fn(pre,cur,index,arr);
+    }
+    return pre;
+};
+
+let arr=[1,2,3,4,5,6];
+let sum=arr.makeReducer((pre,cur,index,arr)=>{
+    return pre+cur;
+});
+console.log(sum);
