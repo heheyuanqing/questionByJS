4bee1236e911e7dc104c2e29008cf93224eda5db
heheyuanqing <2570394541@qq.com>
2018-08-16 09:14:18 +0800

    实现防抖动&节流

--- debounce.js
+++ debounce.js
@@ -1,5 +1,5 @@
 /*
-* 限制函数下次调用之前需等待的时间间隔
+* 防抖动：限制函数下次调用之前需等待的时间间隔
 * */
 function debounce(fn,delay){
     let timer = null;
@@ -14,4 +14,19 @@ function debounce(fn,delay){
 }
 
 /*
+* 节流：允许函数在规定时间内执行一次
 * */
+function throttle(fn,delay){
+    let timer=null;
+
+    return function (){
+        let context=this,
+            arg=arguments;
+        if(!timer){
+            timer=setTimeout(function (){
+                fn.apply(context,arg);
+                timer=null;
+            },delay);
+        }
+    }
+}
